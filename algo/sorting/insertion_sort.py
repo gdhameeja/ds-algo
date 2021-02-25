530eb572de984346a1fb69b1597f2887d241a25b
Gaurav Dhameeja <gaurav.dhameeja@fiftyfive.io>
2021-02-25 09:47:16 +0530

    algo/sort: Modified insertion sort
    Made insertion sort to be simple and easy to understand

--- algo/sorting/insertion_sort.py
+++ algo/sorting/insertion_sort.py
@@ -1,21 +1,22 @@
 #!/usr/bin/env python3
 
 def insertion_sort(arr):
-    i = 1
-    while i < len(arr):
+    for i in range(1, len(arr)):
         key = arr[i]
-        j = i-1
+        j = i - 1
         while j >= 0 and arr[j] > key:
-            arr[j+1] = arr[j]
+            arr[j + 1] = arr[j]
             j -= 1
-        arr[j+1] = key
-        i += 1
+        arr[j + 1] = key
     return arr
 
 if __name__ == "__main__":
-    arr = [5, 3, 11, 101, 1, 7]
+    arr1 = [5, 3, 11, 101, 1, 7]
+    arr2 = [1, 2, 3, 4, 5]
 
     # unit test
-    assert insertion_sort(arr) == [1, 3, 5, 7, 11, 101]
+    assert insertion_sort(arr1) == [1, 3, 5, 7, 11, 101]
+    assert insertion_sort(arr2) == [1, 2, 3, 4, 5]
 
-    print(insertion_sort(arr))
+    print(insertion_sort(arr1))
+    print(insertion_sort(arr2))
